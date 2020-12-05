5c53fb5d5d9da6c2c295f8375a81feddcb01c015
Michael Siu <michaeltsiu13@gmail.com>
2020-12-05 01:37:50 -0800

    Completed Binary search trees

---
+++ 10. Binary Search Trees/Binary Search Trees.js
@@ -0,0 +1,130 @@
+/*
+What is a tree?
+  - A data structure that consists of nodes in a parent/child relationship
+  - Lists: linear
+  - Trees: nonlinear
+
+Kinds of trees
+  - Tree
+  - Binary Tree
+    - At most 2 children
+    - Binary Search Tree
+      - Kept in an order
+      - Left child is smaller than parent
+      - Right child is larger than parent
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while(true) {
+        if (value === current.value) return `${current.value} is a duplicate`;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          }
+          current = current.left
+        } else {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null)  return false;
+    let current = this.root,
+        found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+  BFS() {
+    let data = [],
+        queue = [this.root];
+    while(queue.length) {
+      const node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder() { // preOrder
+    let data = [],
+        queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      data.unshift(node.value);
+      if (node.left) queue.unshift(node.left);
+      if (node.right) queue.unshift(node.right);
+    }
+    return data;
+  }
+  DFSPostOrder() { //postOder
+    let data = [],
+        queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      if (node.left) queue.unshift(node.left);
+      if (node.right) queue.unshift(node.right);
+      data.unshift(node.value);
+    }
+    return data;
+  }
+  DFSInOrder() { // inOrder
+    let data = [],
+    queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      if (node.left) queue.unshift(node.left);
+      data.unshift(node.value);
+      if (node.right) queue.unshift(node.right);
+    }
+    return data;
+  }
+}
+
+//      10
+//    6   15
+//  3  8    20
+
+let tree = new BinarySearchTree;
+tree.insert(10)
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8)
+tree.insert(20);
+
+// Big O of BST
+// - Insertion: O(log n)
+// - Searching: O(log n)
+// (not guaranteed)
---
+++ 10. Binary Search Trees/Tree BFS & DFS.js
@@ -0,0 +1,101 @@
+// Visit every node one time
+// Trees not ordered
+
+/*
+Breadth-first Search
+ - Work across the tree in a row before moving down
+ - Steps (Iteratively)
+  1. Create a queue (this can be an array) and a variable to store the values of nodes visitied
+  2. Place the root node in the queue
+  3. Loop as long as there is anything in the queue
+    - Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+    - If there is a left property on the node dequeued - add it to the queue
+    - If there is a right property on the node dequeued - add it to the queue
+    - Return the node that stored all the values
+
+Depth-first Search
+  - Working downward in the tree like a column
+  - Preorder/Postorder
+
+*/
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  BFS() {
+    let data = [],
+        queue = [this.root];
+    while(queue.length) {
+      const node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder() { // preOrder
+    // let data = [];
+    // function traverse(node) {
+    //   data.push(node.value)
+    //   if (node.left) traverse(node.left);
+    //   if (node.right) traverse(node.right);
+    // }
+    // traverse(this.root);
+    // return data;
+    let data = [],
+        queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      data.unshift(node.value);
+      if (node.left) queue.unshift(node.left);
+      if (node.right) queue.unshift(node.right);
+    }
+    return data;
+  }
+  DFSPostOrder() { //postOder
+    // let data = [];
+    // function traverse(node) {
+    //   if (node.left) traverse(node.left);
+    //   if (node.right) traverse(node.right);
+    //   data.push(node.value)
+    // }
+    // traverse(this.root);
+    // return data;
+    let data = [],
+        queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      if (node.left) queue.unshift(node.left);
+      if (node.right) queue.unshift(node.right);
+      data.unshift(node.value);
+    }
+    return data;
+  }
+  DFSInOrder() { // inOrder
+    // let data = [];
+    // function traverse(node) {
+    //   if (node.left) traverse(node.left);
+    //   data.push(node.value);
+    //   if (node.right) traverse(node.right);
+    // }
+    // traverse(this.root);
+    // return data;
+    let data = [],
+    queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      if (node.left) queue.unshift(node.left);
+      data.unshift(node.value);
+      if (node.right) queue.unshift(node.right);
+    }
+    return data;
+  }
+}
+
+// QUEUE: []
+// DATA: [10, 6, 15, 3, 8, 20]
+
+//    10
+//  6   15
+// 3 8    20
